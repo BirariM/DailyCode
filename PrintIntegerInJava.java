ece01e90b7276120a691a6169f7b6e8db5a06111
BirariM <117138008+BirariM@users.noreply.github.com>
2023-02-27 23:59:27 +0530

    integer

---
+++ PrintIntegerInJava.java
@@ -0,0 +1,18 @@
+package ds;
+
+import java.util.Iterator;
+
+public class PrintIntegerInJava {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c; //Declaring a varibale
+		
+		//Using for loop to repeat instructions executions
+		
+		for(c=1; c<=10; c++) {
+			System.out.println(c);
+		}
+	
+	}
+}
