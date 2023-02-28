230aa8b4f4b0b245944d4883627a87ff99f29585
BirariM <117138008+BirariM@users.noreply.github.com>
2023-03-01 00:02:17 +0530

    Add files via upload

---
+++ CommandLineArgumentInJava.java
@@ -0,0 +1,20 @@
+package ds;
+
+public class CommandLineArgumentInJava {
+
+		public static void main(String[] args) {
+			/*
+			 * compile by > javac CommandLineArgumentInJava.java  
+				   run by > java CommandLineArgumentInJava MayurBirari 
+			 * 
+			 * OR
+			 * go to RUN CONFIGURATIONS->ARGUMENTS->RUN
+			 */
+			for(String t: args) {
+				
+				System.out.println(t);
+				
+			}
+			
+		}
+}
